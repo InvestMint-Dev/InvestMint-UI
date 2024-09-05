02e43916a832df680469438924cd173bfb9bcde7
Leanne <sooyeunleanne@gmail.com>
2024-09-05 09:42:46 -0400

    create-account-page-1

---
+++ src/pages/create-account-page/create-account-page-1/create-account-page-1.js
@@ -0,0 +1,23 @@
+import './../create-account-page.css';
+
+export const CreateAccountPage1 = () => {
+    return (
+        <div className='createaccountform-container'>
+            <h1 className='form-heading'>Create Your Account</h1>
+            <div className='createaccountform'>
+                <textarea className='form-textarea' placeholder='Email'></textarea>
+                <textarea className='form-textarea' placeholder='Password'></textarea>
+                <textarea className='form-textarea' placeholder='Confirm Password'></textarea>
+
+                <button className='form-submit-button'>
+                    Submit
+                </button>
+
+                <div className='form-option-2-container'> 
+                    <span className="form-label">New Member? </span>
+                    <a href="" className="form-link">Sign Up here</a>
+                </div>
+            </div>
+        </div>
+    );
+}
---
+++ src/pages/create-account-page/create-account-page.js
@@ -0,0 +1,8 @@
+import { CreateAccountPage1 } from './create-account-page-1/create-account-page-1';
+import './create-account-page.css';
+
+export const CreateAccountPage = () => {
+    return (
+        <CreateAccountPage1/>
+    );
+}
