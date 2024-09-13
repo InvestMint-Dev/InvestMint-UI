933f44825426df7db7ae35eea0245be5d698d147
Leanne <sooyeunleanne@gmail.com>
2024-09-13 12:18:22 -0400

    trigger errors in log in page only when submit button is clicked

--- src/pages/log-in-page/log-in-page.js
+++ src/pages/log-in-page/log-in-page.js
@@ -10,36 +10,11 @@ export const LogInPage = () => {
     const [formData, setFormData] = useState({
         email: "",
         password: ""
-      });
-    
-      const [errors, setErrors] = useState({});
-      const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
-
-      const validateForm = () => {
-        const validationErrors = validateLogInFields(formData, 'login');
-        setErrors(validationErrors);
-        return Object.keys(validationErrors).length === 0;
-    };
-    
-    const handleSubmit = (e) => {
-      e.preventDefault();
-
-      if (validateForm()) {
-          // Submit the form if no errors
-          console.log("Form submitted successfully!");
-      }
-  };
-
-  const handleChange = (e) => {
-      const { name, value } = e.target;
-
-      setFormData((prevFormData) => {
-        const updatedFormData = { ...prevFormData, [name]: value };
-        const validationErrors = validateLogInFields(updatedFormData, 'login');
-        setErrors(validationErrors);
-        return updatedFormData;
-    });
-  };
+        });
+        
+    const [errors, setErrors] = useState({});
+    const [showPassword, setShowPassword] = useState(false); // State to toggle password visibility
+    const [submitButtonClicked, setSubMitButtonClicked] = useState(false);
 
     useEffect(() => {
         // Runs after the component mounts
@@ -50,61 +25,87 @@ export const LogInPage = () => {
     }, []); // Empty dependency array means this effect runs once after the initial render
 
 
+    const validateForm = () => {
+        const validationErrors = validateLogInFields(formData, 'login');
+        setErrors(validationErrors);
+        return Object.keys(validationErrors).length === 0;
+    };
+    
+    const handleSubmit = (e) => {
+        setSubMitButtonClicked(true);
+        e.preventDefault();
+
+        if (validateForm()) {
+            // Submit the form if no errors
+            console.log("Form submitted successfully!");
+            setSubMitButtonClicked(false);
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData((prevFormData) => {
+        const updatedFormData = { ...prevFormData, [name]: value };
+        const validationErrors = validateLogInFields(updatedFormData, 'login');
+        setErrors(validationErrors);
+        return updatedFormData;
+    })};
+
     return (
-      <div>
-      <img className='top-left-logo-display' src={bigLeafLogo} alt="InvestMint Logo" />
+        <div>
+            <img className='top-left-logo-display' src={bigLeafLogo} alt="InvestMint Logo" />
 
-      <div className='log-in-form-container'>
-          <h1 className='form-heading'>Log-in</h1>
-          <form className='log-in-form' onSubmit={handleSubmit}>
-              {/* email input */}
-              {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
-              <input 
-                  className='form-textarea form-textarea-full' 
-                  placeholder='Email'
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  style={{
-                      border: errors.email ? "2px solid red" : "none"
-                  }} />
+            <div className='log-in-form-container'>
+                <h1 className='form-heading'>Log-in</h1>
+                <form className='log-in-form' onSubmit={handleSubmit}>
+                    {/* email input */}
+                    {(errors.email && submitButtonClicked) && <p style={{ color: "red" }}>{errors.email}</p>}
+                    <input 
+                        className='form-textarea form-textarea-full' 
+                        placeholder='Email'
+                        type="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        style={{
+                            border: (errors.email && submitButtonClicked) ? "2px solid red" : "none"
+                        }} />
 
-              {/* password input */}
-              {errors.password && <p style={{ color: "red" }}>{errors.password}</p>}
-              <div className='password-container'>
-                <input 
-                    type={showPassword ? "text" : "password"} // Toggling between text and password
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className='form-textarea form-textarea-full password-textarea' 
-                    placeholder='Password'
-                    style={{
-                        border: errors.password ? "2px solid red" : "none"
-                    }} >
-                    </input>
-                <button type="button" className='show-password-button' onClick={() => setShowPassword(!showPassword)}>
-                    {showPassword ? <img src={openEye}/> : <img src={closedEye}/>}
-                </button>
-              </div>
+                    {/* password input */}
+                    {(errors.password && submitButtonClicked) && <p style={{ color: "red" }}>{errors.password}</p>}
+                    <div className='password-container'>
+                        <input 
+                            type={showPassword ? "text" : "password"} // Toggling between text and password
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
+                            className='form-textarea form-textarea-full password-textarea' 
+                            placeholder='Password'
+                            style={{
+                                border: (errors.password && submitButtonClicked) ? "2px solid red" : "none"
+                            }} >
+                            </input>
+                        <button type="button" className='show-password-button' onClick={() => setShowPassword(!showPassword)}>
+                            {showPassword ? <img src={openEye}/> : <img src={closedEye}/>}
+                        </button>
+                    </div>
 
-              <div className="form-option-1-container">
-                  <input className="form-checkbox" type="checkbox" id="rememberUser" name="rememberUser" value="rememberUser" />
-                  <span className="form-label">Remember Me</span>
-                  <a href="" className="form-link">Forgot password</a>
-              </div>
+                    <div className="form-option-1-container">
+                        <input className="form-checkbox" type="checkbox" id="rememberUser" name="rememberUser" value="rememberUser" />
+                        <span className="form-label">Remember Me</span>
+                        <a href="" className="form-link">Forgot password</a>
+                    </div>
 
-              <button type="submit" className='form-submit-button'>
-                  Submit
-              </button>
+                    <button type="submit" className='form-submit-button'>
+                        Submit
+                    </button>
 
-              <div className='form-option-2-container'> 
-                  <span className="form-label">New Member? </span>
-                  <a href="/create-account" className="form-link">Sign Up here</a>
-              </div>
-          </form>
-      </div>
-  </div>
-    );
-}
+                    <div className='form-option-2-container'> 
+                        <span className="form-label">New Member? </span>
+                        <a href="/create-account" className="form-link">Sign Up here</a>
+                    </div>
+                </form>
+            </div>
+        </div>);
+    }
