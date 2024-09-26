7481f163d7fa830409eb581f25a9b6d4f62b4b9c
Leanne <sooyeunleanne@gmail.com>
2024-09-26 14:57:55 -0400

    adding log out logic

--- src/pages/dashboard-page/dashboard-page.js
+++ src/pages/dashboard-page/dashboard-page.js
@@ -1,9 +1,25 @@
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 import './dashboard-page.css';
 
 export const DashboardPage = () => {
+    const { logout } = useAuth0();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        // Log out from Auth0 and then navigate to the login page
+        logout({
+            returnTo: window.location.origin // Redirects to the home page or login after logging out
+        });
+        navigate('/log-in'); // Navigate to login page after logout
+    };
+
     return (
         <div>
-            <button className='logout-button'>Log Out</button>
+        <button className='logout-button' onClick={handleLogout}>
+            Log Out
+        </button>
         </div>
     );
-}
+};
