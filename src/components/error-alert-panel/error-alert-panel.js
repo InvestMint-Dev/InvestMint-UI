ef653a3da04b14bef2da832933006e0a40079b6c
Leanne <sooyeunleanne@gmail.com>
2024-10-17 15:51:14 -0400

    changing error alert panel message

--- src/components/error-alert-panel/error-alert-panel.js
+++ src/components/error-alert-panel/error-alert-panel.js
@@ -3,7 +3,7 @@ import './error-alert-panel.css';
 export const ErrorAlertPanel = ({ className }) => {
     return (
         <div className={`error-alert-panel ${className}`}>
-            <p>Some required information is missing.</p> 
+            <p>Error:</p> 
             <p>Please review and complete all fields.</p>
         </div>
     );
