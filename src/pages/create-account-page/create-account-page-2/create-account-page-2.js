76de02cec202b63eba2879cff1652ddab45c5b78
Leanne <sooyeunleanne@gmail.com>
2024-10-17 16:50:28 -0400

    styling and fixing logic for page 2

--- src/pages/create-account-page/create-account-page-2/create-account-page-2.js
+++ src/pages/create-account-page/create-account-page-2/create-account-page-2.js
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { ErrorAlertPanel } from '../../../components/error-alert-panel/error-alert-panel';
 import { CreateAccountSidebar } from '../create-account-sidebar/create-account-sidebar';
 import '../create-account-page.css';
+import './create-account-page-2.css';
 
 import { handleKeyDown } from '../../../utils/utils';
 import { validateTwoFactorAuth } from '../../../validators/validators';
@@ -108,58 +109,69 @@ export const CreateAccountPage2 = () => {
 
             <CreateAccountSidebar currentPage={3}/>
 
-            <div className='page-3-container'>
+            <div className='page-2-container'>\
+                <h1 className="form-heading">Two-Factor Authentication</h1>
+                
                 <div className='two-factor-auth-form'>
-                    <select 
-                        style={{ border: (errors.securityQuestion1 && nextButtonClicked) ? "2px solid #61b090" : "none" }} 
-                        id= "securityQuestion1"
-                        name="securityQuestion1" 
-                        value={formData.securityQuestion1} // Ensure the value is set correctly
-                        onChange={(e) => handleChange(e)}
-                    >
-                        <option value="">Select Security Question</option>
-                        <option value="firstClient">What was the name of your company's first major client?</option>
-                        <option value="foundingYear">In what year was your company founded?</option>
-                        <option value="firstOffice">What city was your company’s first office located in?</option>
-                        <option value="founder">What is the name of your company’s founder?</option>
-                        <option value="firstProject">What was the name of your company’s first project?</option>
-                        <option value="companyMotto">What is your company’s motto or mission statement?</option>
-                        <option value="firstProduct">What was the name of your company's first product or service?</option>
-                        <option value="firstEmployee">What is the name of your company’s first employee?</option>
-                        <option value="parentCompany">What is the name of your company’s parent company (if applicable)?</option>
-                    </select>                            
-                    {(errors.securityQuestion1 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityQuestion1}</p>}
+                    <div className='form-select security-questions-select'>
+                        <select 
+                            style={{ border: (errors.securityQuestion1 && nextButtonClicked) ? "2px solid #61b090" : "none" }} 
+                            id= "securityQuestion1"
+                            name="securityQuestion1" 
+                            value={formData.securityQuestion1} // Ensure the value is set correctly
+                            onChange={(e) => handleChange(e)}
+                        >
+                            <option value="">Select Security Question 1</option>
+                            <option value="firstClient">What was the name of your company's first major client?</option>
+                            <option value="foundingYear">In what year was your company founded?</option>
+                            <option value="firstOffice">What city was your company’s first office located in?</option>
+                            <option value="founder">What is the name of your company’s founder?</option>
+                            <option value="firstProject">What was the name of your company’s first project?</option>
+                            <option value="companyMotto">What is your company’s motto or mission statement?</option>
+                            <option value="firstProduct">What was the name of your company's first product or service?</option>
+                            <option value="firstEmployee">What is the name of your company’s first employee?</option>
+                            <option value="parentCompany">What is the name of your company’s parent company (if applicable)?</option>
+                        </select>                            
+                        {(errors.securityQuestion1 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityQuestion1}</p>}
+                    </div>
                     
-                    <div className='form-textarea-container-full'>
-                        <textarea onKeyDown={handleKeyDown}  id="form-textarea" className='form-textarea'  name="securityAnswer1" placeholder='Answer' value={formData.securityAnswer1} onChange={handleChange} style={{ border: (errors.securityAnswer1 && nextButtonClicked) ? "2px solid #61b090" : "none" }}></textarea>
-                        {(errors.securityAnswer1 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityAnswer1}</p>}
+                    {formData.securityQuestion1 && 
+                        <div className='form-textarea-container-full'>
+                            <textarea onKeyDown={handleKeyDown}  id="form-textarea" className='form-textarea'  name="securityAnswer1" placeholder='Answer' value={formData.securityAnswer1} onChange={handleChange} style={{ border: (errors.securityAnswer1 && nextButtonClicked) ? "2px solid #61b090" : "none" }}></textarea>
+                            {(errors.securityAnswer1 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityAnswer1}</p>}
+                        </div>
+                    }
+
+                    <div className='form-select security-questions-select'>
+                        <select 
+                            style={{ border: (errors.securityQuestion2 && nextButtonClicked) ? "2px solid #61b090" : "none" }} 
+                            id= "securityQuestion2"
+                            name="securityQuestion2" 
+                            value={formData.securityQuestion2} // Ensure the value is set correctly
+                            onChange={(e) => handleChange(e)}
+                        >
+                            <option value="">Select Security Question 2</option>
+                            <option value="taxID">What is the last four digits of your company’s tax ID?</option>
+                            <option value="firstSupplier">What was the name of your company’s first supplier?</option>
+                            <option value="firstProductLaunch">In what year did your company launch its first product?</option>
+                            <option value="patentNumber">What is the number of your company’s first patent (if applicable)?</option>
+                            <option value="headquartersNickname">What is the internal nickname for your company's headquarters?</option>
+                            <option value="firstClientContract">What was the name of the company involved in your first major client contract?</option>
+                            <option value="industryAssociation">What is the name of the first industry association your company joined?</option>
+                            <option value="companyAnniversary">What date does your company celebrate as its official anniversary?</option>
+                            <option value="firstBoardMember">What is the name of the first board member in your company?</option>
+                            <option value="annualRevenue">What was your company’s reported annual revenue in its first year of operation?</option>
+                        </select>                            
+                        {(errors.securityQuestion2 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityQuestion2}</p>}
                     </div>
 
-                    <select 
-                        style={{ border: (errors.securityQuestion2 && nextButtonClicked) ? "2px solid #61b090" : "none" }} 
-                        id= "securityQuestion2"
-                        name="securityQuestion2" 
-                        value={formData.securityQuestion2} // Ensure the value is set correctly
-                        onChange={(e) => handleChange(e)}
-                    >
-                        <option value="">Select Security Question</option>
-                        <option value="taxID">What is the last four digits of your company’s tax ID?</option>
-                        <option value="firstSupplier">What was the name of your company’s first supplier?</option>
-                        <option value="firstProductLaunch">In what year did your company launch its first product?</option>
-                        <option value="patentNumber">What is the number of your company’s first patent (if applicable)?</option>
-                        <option value="headquartersNickname">What is the internal nickname for your company's headquarters?</option>
-                        <option value="firstClientContract">What was the name of the company involved in your first major client contract?</option>
-                        <option value="industryAssociation">What is the name of the first industry association your company joined?</option>
-                        <option value="companyAnniversary">What date does your company celebrate as its official anniversary?</option>
-                        <option value="firstBoardMember">What is the name of the first board member in your company?</option>
-                        <option value="annualRevenue">What was your company’s reported annual revenue in its first year of operation?</option>
-                    </select>                            
-                    {(errors.securityQuestion2 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityQuestion2}</p>}
+                    {formData.securityQuestion2 && 
+                        <div className='form-textarea-container-full'>
+                            <textarea onKeyDown={handleKeyDown}  id="form-textarea" className='form-textarea'  name="securityAnswer2" placeholder='Answer' value={formData.securityAnswer2} onChange={handleChange} style={{ border: (errors.securityAnswer2 && nextButtonClicked) ? "2px solid #61b090" : "none" }}></textarea>
+                            {(errors.securityAnswer2 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityAnswer2}</p>}
+                        </div>
+                    }
                     
-                    <div className='form-textarea-container-full'>
-                        <textarea onKeyDown={handleKeyDown}  id="form-textarea" className='form-textarea'  name="securityAnswer2" placeholder='Answer' value={formData.securityAnswer2} onChange={handleChange} style={{ border: (errors.securityAnswer2 && nextButtonClicked) ? "2px solid #61b090" : "none" }}></textarea>
-                        {(errors.securityAnswer2 && nextButtonClicked) && <p style={{ color: '#61b090' }}>{errors.securityAnswer2}</p>}
-                    </div>
                 </div>
 
                 <div className='stepper-container'>
