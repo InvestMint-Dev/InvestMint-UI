b9d09c1b083679bfc6e4274ed27d5247a740938c
Leanne <sooyeunleanne@gmail.com>
2024-10-17 13:12:05 -0400

    setting up a page for two factor authentication

---
+++ src/pages/create-account-page/create-account-page-2/create-account-page-2.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import { CreateAccountSidebar } from '../create-account-sidebar/create-account-sidebar';
+import '../create-account-page.css';
+
+export const CreateAccountPage2 = () => {
+    
+};
