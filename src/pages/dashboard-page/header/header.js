8f9c8d86b9dd4358a41aa66e5d02b10e0d9071c7
Leanne <sooyeunleanne@gmail.com>
2024-12-05 13:08:23 +0900

    header styling

--- src/pages/dashboard-page/header/header.js
+++ src/pages/dashboard-page/header/header.js
@@ -2,13 +2,15 @@ import './header.css';
 
 export const DashboardHeader = () => {
     return (
-        <div>
-            <h3>InvestMint</h3>
-            <button>Dashboard</button>
-            <button>Portfolio</button>
-            <button>Risk</button>
-            <button>Bank</button>
-            <button>Sandbox</button>
+        <div className='header-container'>
+            <b className='logo'>InvestMint</b>
+            <div className='header-nav-container'>
+                <button>Dashboard</button>
+                <button>Portfolio</button>
+                <button>Risk</button>
+                <button>Bank</button>
+                <button>Sandbox</button>
+            </div>
         </div>
     );
 }
