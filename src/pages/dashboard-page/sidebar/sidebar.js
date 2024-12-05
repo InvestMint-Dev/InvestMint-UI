351f1b23c24333692373bf6faef00fa269abf081
Leanne <sooyeunleanne@gmail.com>
2024-12-05 13:17:07 +0900

    sidebar-info-box

--- src/pages/dashboard-page/sidebar/sidebar.js
+++ src/pages/dashboard-page/sidebar/sidebar.js
@@ -3,6 +3,7 @@ import './sidebar.css';
 export const DashboardSidebar = () => {
     return (
         <div className='sidebar-container'>
+            <div className='sidebar-info-box'></div>
         </div>
     )
 }
